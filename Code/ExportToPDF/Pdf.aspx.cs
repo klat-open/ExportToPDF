443150b336ba8e09b8915524a43f2dceac1a1c04
Nguyen Duc Anh <ducanhtk5@gmail.com>
2016-11-04 16:31:53 -0700

    Cap nhat xuat bao cao voi kha nang tu format

--- Code/ExportToPDF/Pdf.aspx.cs
+++ Code/ExportToPDF/Pdf.aspx.cs
@@ -110,7 +110,92 @@ namespace Klat.Example
 
             document.InsertFromExcel(MapPath("~/Uploads/data_merge_row.xlsx"));
 
-            document.Save(MapPath("~/Uploads/data_merge_row.pdf"));
+            ReportColor green = ReportColor.Create(0, 100, 45);
+            ReportColor textHong = ReportColor.Create(230, 184, 183);
+            ReportColor textKhds = ReportColor.Create(99, 37, 35);
+            ReportColor textKhln = ReportColor.Create(118, 147, 60);
+            ReportColor textThds = ReportColor.Create(96, 73, 122);
+            ReportColor textThln = ReportColor.Create(49, 134, 155);
+
+            document.Save(MapPath("~/Uploads/data_merge_row.pdf"), (paragraphs) =>
+            {
+                foreach (IElementRoot paragraph in paragraphs)
+                {
+                    if (paragraph is ITable)
+                    {
+                        ITable table = paragraph as ITable;
+                        ITableRow row1 = table.Rows[0];
+
+                        for (int i = 1; i <= row1.Cells.Count; i++)
+                        {
+                            ITableCell cell = row1.Cells[i - 1];
+                            cell.BackgoundColor = green;
+
+                            if (i >= 3)
+                            {
+                                cell.TextColor = textHong;
+                            }
+                            else
+                            {
+                                cell.TextColor = ReportColor.While;
+                            }
+                        }
+
+                        ITableRow row4 = table.Rows[3];
+                        for (int i = 1; i <= row4.Cells.Count; i++)
+                        {
+                            ITableCell cell = row4.Cells[i - 1];
+                            cell.TextColor = ReportColor.While;
+                            if (i <= 10)
+                            {
+                                cell.BackgoundColor = green;
+                            }
+                            else if (i == 11)
+                            {
+                                cell.BackgoundColor = textKhds;
+                            }
+                            else if (i == 12)
+                            {
+                                cell.BackgoundColor = textKhln;
+                            }
+                            else if (i == 13)
+                            {
+                                cell.BackgoundColor = textThds;
+                            }
+                            else if (i == 14)
+                            {
+                                cell.BackgoundColor = textThln;
+                            }
+                        }
+
+                        ITableRow row5 = table.Rows[4];
+                        for (int i = 1; i <= row5.Cells.Count; i++)
+                        {
+                            ITableCell cell = row5.Cells[i - 1];
+                            cell.TextColor = ReportColor.While;
+
+                            if (i <= 11)
+                            {
+                                cell.BackgoundColor = textKhds;
+                            }
+                            else if (i <= 18)
+                            {
+                                cell.BackgoundColor = textKhln;
+                            }
+                            else if (i <= 29)
+                            {
+                                cell.BackgoundColor = textThds;
+                            }
+                            else
+                            {
+                                cell.BackgoundColor = textThln;
+                            }
+                        }
+                    }
+                }
+
+                return paragraphs;
+            });
         }
     }
 }
